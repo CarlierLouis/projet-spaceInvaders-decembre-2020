f3e102d08e8ee279c155a8366a4a080a8d363312
Louis Carlier <63374771+CarlouMuhan@users.noreply.github.com>
2020-12-04 16:20:44 +0100

    en ==>fr

--- code/database.py
+++ code/database.py
@@ -5,15 +5,13 @@ cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', da
 cursor = cnx.cursor()
 
 
-def create_Table_Profils():
+def creation_table_Profils():
     cursor.execute("CREATE TABLE Profils (id int primary key not null auto_increment,"
-                   "pseudo varchar(255), xp int(255), money int(255))")
+                   "pseudo varchar(255), xp int(255), argent int(255))")
 
 
-def fill_Table_Profils():
-    sql = "INSERT INTO Profils (pseudo, xp, money) VALUES (%s, %s, %s)"
+def remplir_table_Profils():
+    sql = "INSERT INTO Profils (pseudo, xp, argent) VALUES (%s, %s, %s)"
     value = ("test", 12, 123)
     cursor.execute(sql, value)
     cnx.commit()
-
-
