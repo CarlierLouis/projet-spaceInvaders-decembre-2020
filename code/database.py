6d8c4470886989a107015a7bf75bf59ac51176c7
Louis Carlier <63374771+CarlouMuhan@users.noreply.github.com>
2020-12-06 19:04:09 +0100

    interface scores

--- code/database.py
+++ code/database.py
@@ -4,6 +4,8 @@ from mysql import connector
 cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', database='infos')
 cursor = cnx.cursor()
 
+infos_joueurs = []
+
 
 def creation_table_Profils():
     cursor.execute("CREATE TABLE Profils (id int primary key not null auto_increment,"
@@ -12,6 +14,13 @@ def creation_table_Profils():
 
 def remplir_table_Profils():
     sql = "INSERT INTO Profils (pseudo, xp, argent) VALUES (%s, %s, %s)"
-    value = ("test", 12, 123)
+    value = ("Diego", 108, 34)
     cursor.execute(sql, value)
     cnx.commit()
+
+
+def recuperer_infos_joueurs():
+    cursor.execute("SELECT pseudo, xp, argent FROM Profils")
+    for i in cursor.fetchall():
+        infos_joueurs.append(i)
+    return infos_joueurs
--- code/menu-interface.py
+++ code/menu-interface.py
@@ -1,7 +1,28 @@
 from tkinter import *
 from tkinter import messagebox
-from main import jouer
 
+from main import jouer
+from database import recuperer_infos_joueurs
+
+
+# Affichage des scores (fenêtre des scores)
+def fenetre_score():
+    window_score = Tk()
+
+    window_score.title("Scores")
+    window_score.geometry("500x500")
+    window_score.minsize(480, 360)
+    window_score.iconbitmap("score.ico")
+    window_score.config(background='#660000')
+
+    label_scores = Label(window_score, text="Scores", font=("Arial", 35), bg='#660000', fg='white')
+    label_scores = Label(window_score, text=recuperer_infos_joueurs(), font=("Arial", 15), bg='#660000', fg='white')
+    label_scores.pack()
+
+    window_score.mainloop()
+
+
+# M E N U
 # Création de la fenêtre
 window = Tk()
 
@@ -22,7 +43,8 @@ label_titre.pack()
 # Ajout des boutons
 bouton_jouer = Button(frame, text="Jouer", font=("Arial", 25), bg='white', fg='#121517', bd="10", relief="ridge",
                       command=jouer)
-bouton_score = Button(frame, text="Scores", font=("Arial", 25), bg='white', fg='#121517', bd="10", relief="ridge")
+bouton_score = Button(frame, text="Scores", font=("Arial", 25), bg='white', fg='#121517', bd="10", relief="ridge",
+                      command=fenetre_score)
 bouton_boutique = Button(frame, text="Boutique", font=("Arial", 25), bg='white', fg='#121517', bd="10", relief="ridge")
 bouton_jouer.pack(pady=25, fill=X)
 bouton_score.pack(pady=25, fill=X)
