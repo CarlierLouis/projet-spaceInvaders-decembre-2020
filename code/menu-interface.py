b9c8557f79cc4b2fbeaf645a4f029d6f187e74e8
Louis Carlier <63374771+CarlouMuhan@users.noreply.github.com>
2020-12-07 18:36:38 +0100

    commentaires + fenêtre de choix de profils

--- code/menu-interface.py
+++ code/menu-interface.py
@@ -3,24 +3,29 @@ from tkinter import *
 from main import jouer
 from database import recuperer_infos_joueurs
 
+# Choix du profils
+window_profils = Tk()
+
+window_profils.mainloop()
+
 
 # M E N U
 def fenetre_menu():
     # Création de la fenêtre
-    window = Tk()
+    window_menu = Tk()
 
     # Personnalisation de la fenêtre
-    window.title("Space Invaders")
-    window.geometry("1080x720")
-    window.minsize(480, 360)
-    window.iconbitmap("logo.ico")
-    window.config(background='#121517')
+    window_menu.title("Space Invaders")
+    window_menu.geometry("1080x720")
+    window_menu.minsize(480, 360)
+    window_menu.iconbitmap("logo.ico")
+    window_menu.config(background='#121517')
 
-    # Création de la frame (boîte)
-    frame = Frame(window, bg='#121517')
+    # Création de la frame
+    frame = Frame(window_menu, bg='#121517')
 
     # Titre
-    label_titre = Label(window, text="Space Invaders", font=("Arial", 40), bg='#121517', fg='white')
+    label_titre = Label(window_menu, text="Space Invaders", font=("Arial", 40), bg='#121517', fg='white')
     label_titre.pack()
 
     # Ajout des boutons
@@ -39,52 +44,62 @@ def fenetre_menu():
     frame.pack(expand=YES)
 
     # Affichage
-    window.mainloop()
+    window_menu.mainloop()
 
 
-# Affichage des scores (fenêtre des scores)
+# Affichage des SCORES (fenêtre des scores)
 def fenetre_score():
-    window_score = Tk()
+    # Création de la fenêtre
+    window_scores = Tk()
 
-    window_score.title("Scores")
-    window_score.geometry("1200x400")
-    window_score.minsize(480, 360)
-    window_score.iconbitmap("score.ico")
-    window_score.config(background='#660000')
+    # Personnalisation de la fenêtre
+    window_scores.title("Scores")
+    window_scores.geometry("1200x400")
+    window_scores.minsize(480, 360)
+    window_scores.iconbitmap("score.ico")
+    window_scores.config(background='#660000')
 
-    frame_scores = Frame(window_score, bg='#660000')
-    frame_scores_retour = Frame(window_score, bg='#660000')
+    # Création des frames
+    frame_scores = Frame(window_scores, bg='#660000')
+    frame_scores_retour = Frame(window_scores, bg='#660000')
 
-    label_scores = Label(window_score, text="Scores", font=("Arial", 35), bg='#660000', fg='white')
-    label2_scores = Label(window_score, text="{Pseudo, xp, argent}\n", font=("Arial", 15), bg='#660000', fg='black')
+    # Ajout des textes et boutons
+    label_scores = Label(window_scores, text="Scores", font=("Arial", 35), bg='#660000', fg='white')
+    label2_scores = Label(window_scores, text="{Pseudo, xp, argent}\n", font=("Arial", 15), bg='#660000', fg='black')
     label3_scores = Label(frame_scores, text=recuperer_infos_joueurs(), font=("Arial", 15), bg='#660000', fg='white')
     bouton_scores_retour = Button(frame_scores_retour, text="retour", font=("Arial", 15), bg='#660000', fg='white',
-                                  command=window_score.destroy)
+                                  command=window_scores.destroy)
 
     label_scores.pack()
     label2_scores.pack()
     label3_scores.pack()
     bouton_scores_retour.pack()
 
+    # Ajout des frames
     frame_scores.pack()
     frame_scores_retour.pack(pady=25, fill=X, side=BOTTOM)
 
-    window_score.mainloop()
+    # Affichage
+    window_scores.mainloop()
 
 
-# Affichage de la boutique (fenêtre de la boutique)
+# Affichage de la BOUTIQUE (fenêtre de la boutique)
 def fenetre_boutique():
+    # Création de la fenêtre
     window_boutique = Tk()
 
+    # Personnalisation de la fenêtre
     window_boutique.title("Boutique")
     window_boutique.geometry("500x500")
     window_boutique.minsize(480, 360)
     window_boutique.iconbitmap("boutique.ico")
     window_boutique.config(background='#660000')
 
+    # Création des frames
     frame_boutique = Frame(window_boutique, bg='#660000')
     frame_boutique_retour = Frame(window_boutique, bg='#660000')
 
+    # Ajout des textes et boutons
     label_boutique = Label(window_boutique, text="Boutique", font=("Arial", 35), bg='#660000', fg='white')
     label2_boutique = Label(frame_boutique, text="", font=("Arial", 15), bg='#660000', fg='white')
     bouton_boutique_retour = Button(frame_boutique_retour, text="retour", font=("Arial", 15), bg='#660000', fg='white',
@@ -93,10 +108,9 @@ def fenetre_boutique():
     label2_boutique.pack()
     bouton_boutique_retour.pack()
 
+    # Ajout des frames
     frame_boutique.pack()
     frame_boutique_retour.pack(pady=25, fill=X, side=BOTTOM)
 
+    # Affichage
     window_boutique.mainloop()
-
-
-fenetre_menu()
