e7f6dfc63b2a68de474d80bd77c892c551394661
Disdix10 <44231661+Disdix10@users.noreply.github.com>
2020-12-11 19:14:35 +0100

    deplacement alien
    déplacement des aliens presque fini juste à tester si ils sont aux niveau du joueur ou pas
    Ensuite début du tir du joueur

--- code/classes.py
+++ code/classes.py
@@ -9,10 +9,11 @@ class Entite:
         self.vie = vie
         self.pas = pas
         self.img = img
+        self.tir = ''
         canvas.move(self.img, x, y)
 
-    def tir(self):
-        self.tir = Tir()
+    def tir(self, pas, canvas):
+        self.tir = Tir(self.x, self.y, pas, canvas)
 
 
 class Joueur(Entite):
@@ -24,39 +25,41 @@ class Joueur(Entite):
         self.argents = 0
 
     def deplacement(self, canvas):
-        if 900 >= self.x >= 100:
+        if 956.5 >= self.x >= 56.5:
             canvas.move(self.img, 10 * self.pas, 0)
             self.x = self.x + (10 * self.pas)
-        if self.x < 100:
+        if self.x < 56.5:
             canvas.move(self.img, 10 * -self.pas, 0)
-            self.x = 106.5
-        if self.x > 900:
+            self.x = 56.5
+        if self.x > 956.5:
             canvas.move(self.img, 10 * -self.pas, 0)
-            self.x = 896.5
+            self.x = 956.5
         return canvas
 
 
 class Alien(Entite):
     def __init__(self, x, y, vie, pas, img, canvas):
         Entite.__init__(self, x, y, vie, pas, img, canvas)
+        self.detruit = False
 
     def deplacement(self, canvas, x, y):
-        self.x += x
+        self.x += x * self.pas
         self.y += y
-        canvas.move(self.img, x*self.pas, y)
+        canvas.move(self.img, x * self.pas, y)
         return canvas
 
 
 class Tir:
 
-    def __init__(self, x, y, pas, image):
+    def __init__(self, x, y, pas, canvas):
         self.x = x
         self.y = y
         self.pas = pas
-        self.image = image
+        self.image = canvas.create_rectangle(0, 0, -10, -20, fill='Purple')
+        canvas.move(self.image, x, y)
 
     def deplacement(self):
-        pass
+        return 'lol'
 
     def colission(self):
         pass
--- code/jeu.py
+++ code/jeu.py
@@ -4,9 +4,8 @@ from tkinter import *
 
 def clavier(event):
     touche = event.keysym
-
+    global canvas
     if touche == "Right":
-        global canvas
         ship.pas = 1
         canvas = ship.deplacement(canvas)
 
@@ -14,42 +13,50 @@ def clavier(event):
         ship.pas = -1
         canvas = ship.deplacement(canvas)
 
+    elif touche == "space":
+        try:
+            ship.tir(-1, canvas)
+        except:
+            pass
+
 
 def deplacement_alien(aliens):
     global canvas
-
     for i in range(len(aliens)):
-        print(i)
-        for j in range(len(aliens[i])):
-            canvas = aliens[i][j].deplacement(canvas, 5, 0)
+        if aliens[i].x >= 956.5:
+            for j in range(len(aliens)):
+                aliens[j].pas = -1
+                aliens[j].deplacement(canvas, 0, 5)
+        elif aliens[i].x <= 56.5:
+            for j in range(len(aliens)):
+                aliens[j].pas = 1
+                aliens[j].deplacement(canvas, 0, 5)
+        canvas = aliens[i].deplacement(canvas, 0.5, 0)
 
-    canvas.after(1000, deplacement_alien, aliens)
+    canvas.after(5, deplacement_alien, aliens)
 
 
 fenetre = Tk()
 fenetre['background'] = 'black'
 fenetre.minsize(1000, 500)
 fenetre.maxsize(1000, 500)
-
 canvas = Canvas(fenetre, width=1000, height=500)
+canvas.config(bg='black')
 canvas.pack()
 
 # img_ship = canvas.create_image(-33, -28, anchor=NW, image=PhotoImage(file="ship.gif"))
-img_ship = canvas.create_rectangle(0, 0, -33, -28, fill='Black')
-ship = classes.Joueur(516.5, 495, 1, 5, img_ship, canvas)
-aliens_canvas = [[0] * 10] * 5
-aliens = [[0] * 10] * 5
+ship = classes.Joueur(516.5, 495, 1, 5, canvas.create_rectangle(0, 0, -33, -28, fill='white'), canvas)
+aliens = []
 
 y = 0
+x = 106.5
+for i in range(50):
+    x += 50
+    if i % 10 == 0:
+        y += 30
+        x = 106.5
+    aliens.append(classes.Alien(x, y, 1, 1, canvas.create_rectangle(0, 0, -33, -28, fill='Red'), canvas))
 
-for i in range(len(aliens)):
-    y += 30
-    x = 106.5
-    for j in range(len(aliens[i])):
-        x += 50
-        aliens_canvas[i][j] = canvas.create_rectangle(0, 0, -33, -28, fill='Red')
-        aliens[i][j] = classes.Alien(x, y, 1, 1, aliens_canvas[i][j], canvas)
-print(aliens)
 deplacement_alien(aliens)
 fenetre.bind("<Key>", clavier)
 fenetre.mainloop()
